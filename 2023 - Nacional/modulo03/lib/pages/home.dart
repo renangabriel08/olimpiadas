eac6c341009dc8621d23eb04a968c147faf62780
renangabriel08 <renan.gab153@gmail.com>
2024-03-08 17:11:53 -0300

    :sparkles: (feat): Adicionando tela inicial
    Neste commit foi adicionada a tela incial com dados do usuário logado

--- 2023 - Nacional/modulo03/lib/pages/home.dart
+++ 2023 - Nacional/modulo03/lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:modulo03/models/user.dart';
+import 'package:modulo03/styles/styles.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,6 +12,82 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    print(User.user);
+    print(TimeOfDay.now().period);
+
+    return Scaffold(
+      body: Container(
+        width: width,
+        height: height,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Textos.txt1('Inicio', TextAlign.start, Cores.verde3),
+                  IconButton(
+                    onPressed: () => Navigator.pushNamed(context, '/login'),
+                    icon: Icon(
+                      Icons.logout,
+                      color: Cores.verde3,
+                    ),
+                  ),
+                ],
+              ),
+              // ignore: prefer_const_constructors
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [],
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(999),
+                      border: Border.all(width: 3, color: Cores.verde3),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(999),
+                      child: Image.network(User.user!['fotoPerfil']),
+                    ),
+                  ),
+                  Container(width: width * .03),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      User.user!['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'] !=
+                              'Comum'
+                          ? const Icon(Icons.crop)
+                          : Container(),
+                      Textos.txt1(
+                        User.user!['name'],
+                        TextAlign.start,
+                        Cores.verde3,
+                      ),
+                      Textos.txt1(
+                        User.user!['email'],
+                        TextAlign.start,
+                        Cores.verde3,
+                      ),
+                      Textos.txt1(
+                        User.user!['dr'],
+                        TextAlign.start,
+                        Cores.verde3,
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
